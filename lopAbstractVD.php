b8a0aa4d48015f44fcaedfb767a9046a6ed34426
luka <thienthan_sao29@yahoo.com>
2016-01-06 10:14:42 +0000

    interface example

--- lopAbstractVD.php
+++ lopAbstractVD.php
@@ -6,6 +6,7 @@
 * abstract class van duoc xem la mot lop bt va co day du tinh nang cua mot lop bt
 * vai tro abstract: tao ra mot lop ma o do dinh nghia cac phuong thuc chung cho nhieu lop khac
 * cac lop ke thua lop abstract co the dinh nghia lai cac phuong thuc chung tuy vao nhiem vu cu the cua no
+* la lop cha cho cac lop con co chung ban chat-> moi lop con chi ke thua 1 lop abstract
 */
 
 abstract class employee 
