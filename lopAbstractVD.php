2e6806f2bf82983b0b6c882d2b3598299ba6ec52
luka <thienthan_sao29@yahoo.com>
2016-01-06 09:35:18 +0000

    abstract class

---
+++ lopAbstractVD.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* abstract class: la lop chua cac phuong thuc truu tuong
+* cac lop ke thua lop abstract phai dn lai cac phuong thuc truu tuong
+* mot class bt chi duoc ke thua mot lop abstract
+* abstract class van duoc xem la mot lop bt va co day du tinh nang cua mot lop bt
+* vai tro abstract: tao ra mot lop ma o do dinh nghia cac phuong thuc chung cho nhieu lop khac
+* cac lop ke thua lop abstract co the dinh nghia lai cac phuong thuc chung tuy vao nhiem vu cu the cua no
+*/
+
+abstract class employee 
+{
+	protected $empname;
+	protected $empage;
+
+	public function setData($empname, $empage)
+	{
+		$this->empname = $empname;
+		$this->empage = $empage;
+	}
+
+	abstract function outputData();
+}
+
+/**
+* 
+*/
+class EmployeeData extends employee
+{
+	
+	public function __construct($name, $age)
+	{
+		$this->setData($name, $age);
+	}
+
+	public function outputData()
+	{
+		echo $this->empname;
+		echo $this->empage;
+	}
+}
+
+$a = new EmployeeData("giang", "25");
+$a->outputData();
+?>
